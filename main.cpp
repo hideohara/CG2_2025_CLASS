b0ef470672a3e13e904ecc9c5687565dfd8f3eea
ohara <ohara@g.neec.ac.jp>
2025-04-13 11:01:39 +0900

    00_04_資料_ログを出そう

--- main.cpp
+++ main.cpp
@@ -1,5 +1,44 @@
 #include <Windows.h>
 #include <cstdint>
+#include <string>
+#include <format>
+
+#include <windows.h>
+#include <string>
+
+std::wstring ConvertString(const std::string& str) {
+    if (str.empty()) {
+        return std::wstring();
+    }
+
+    auto sizeNeeded = MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), NULL, 0);
+    if (sizeNeeded == 0) {
+        return std::wstring();
+    }
+    std::wstring result(sizeNeeded, 0);
+    MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), &result[0], sizeNeeded);
+    return result;
+}
+
+std::string ConvertString(const std::wstring& str) {
+    if (str.empty()) {
+        return std::string();
+    }
+
+    auto sizeNeeded = WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), NULL, 0, NULL, NULL);
+    if (sizeNeeded == 0) {
+        return std::string();
+    }
+    std::string result(sizeNeeded, 0);
+    WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), result.data(), sizeNeeded, NULL, NULL);
+    return result;
+}
+
+void Log(const std::string& message) {
+    OutputDebugStringA(message.c_str());
+}
+
+
 
 // ウィンドウプロシージャ
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
@@ -21,8 +60,7 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
-    // 出力ウィンドウへの文字出力
-    OutputDebugStringA("Hello,DirectX!\n");
+
 
     WNDCLASS wc{};
     // ウィンドウプロシージャ
@@ -76,6 +114,19 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
         }
     }
 
+    // 出力ウィンドウへの文字出力
+    OutputDebugStringA("Hello,DirectX!\n");
+
+    Log("aaaaaaa\n");
+    std::string str0{ "STRING!!!\n" };
+    Log(str0);
+
+    // 変数から型を推論してくれる
+    Log(std::format("enemyHp:{}, texturePath:{}\n", 4, 6));
+
+    // wstring->string
+    Log(ConvertString(std::format(L"WSTRING{}\n", 55)));
+    Log(ConvertString(std::format(L"WSTRING{}\n", L"aaaaaaaa")));
 
 
 
